e673ee307a29e0531c96b76666d992c2965066e2
pengtaoman@163.com <pengtaoman@163.com>
2019-11-13 22:45:49 +0800

    Initial Commit

---
+++ demo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  demo
+//
+//  Created by 彭涛 on 2019/11/13.
+//  Copyright © 2019 彭涛. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
